bdaf8f1b2cbd7f80d27b6e37c8fb343bb601f0a2
vanjmg <engr.vanjmg@gmail.com>
2021-10-27 21:07:36 +0800

    Finish Confectionary Activity

--- confectionary.rb
+++ confectionary.rb
@@ -1,22 +1,31 @@
-module Bake_at_350deg_for_25min
-    def bake
-        puts "Baking at 350 degrees for 25minutes"
+module Bake_350deg_25min
+    def prepare
+        puts "Baking at 350 degrees for 25 minutes"
+    end
+end
 
 class Confection
 end
 
 class Cupcake < Confection
-include Bake_at_350deg_for_25min
+include Bake_350deg_25min
     def apply_frosting
         puts "Applying Frosting"
     end
 end
 
 class Banana_Cake < Confection
-include Bake_at_350deg_for_25min
+include Bake_350deg_25min
 end
 
+puts "Cupcake is"
 cc = Cupcake.new
+puts cc.prepare
 
+puts "Cupcake is now.."
+puts cc.apply_frosting
 
+puts "Banana Cake is.."
+bc = Banana_Cake.new
+puts bc.prepare
 
